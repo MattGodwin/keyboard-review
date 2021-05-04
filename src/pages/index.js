2c32954f935baacbdfaeacd52d4db8a3af46f33b
Matt <68249412+MattGodwin@users.noreply.github.com>
2021-05-04 18:26:37 +0100

    Customised text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Enmatt's Keyboard Reviews."
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -15,19 +15,18 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
+
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>I'm Enmatt and this is my keyboard review blog!</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          I aim to give entertaining honest reviews about all things keyboards.
         </p>
-        <p>Now go build something great!</p>
+        <p>Hope you enjoy!</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
