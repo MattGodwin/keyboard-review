508124714e9ab1ba627a05044ff584edbffcdf6b
Matt <68249412+MattGodwin@users.noreply.github.com>
2021-05-08 17:37:39 +0100

    fix ga4 lol

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        trackingId: `G-N24DSFK2NK`,
+        trackingId: `UA-192127705-2`,
       },
     },
     {
